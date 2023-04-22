d430e60df651ec1eac26a71dd78fc7fb4232abf8
Louijie Compo <louijie.compo@gmail.com>
2023-04-22 17:28:39 +0800

    Relocated code into .py

--- practice/bigram.py
+++ practice/bigram.py
@@ -1,8 +1,77 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import wget
+from pathlib import Path
+
+# hyperparameters
+batch_size = 4 # how many independent sequences will we process in parallel
+block_size = 8 # what is the maximum context length for predictions
+max_iters = 3000
+eval_interval = 300
+learning_rate = 1e-2
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+eval_iters = 200
 torch.manual_seed(1337)
 
+# Checks if dataset was already downloaded. Skips the download routine if dataset was detected.
+dataset_path = Path.cwd() / 'input.txt'
+if not dataset_path.exists():
+    url = 'https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt'
+    filename = wget.download(url)
+    print(f'Done downloading dataset {filename} from {url}')
+else:
+    print("Dataset already existed hence skipping download.")
+
+# Read data
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+    print(f'Length of dataset in characters: {len(text)}')
+    
+# Get the unique characters in this text
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+
+# Tokenization
+# Many ways to do this, you can use Sentencepiece by goole or tiktoken. 
+stoi = {ch:i for i,ch in enumerate(chars)}
+itos = {i:ch for i,ch in enumerate(chars)}
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
+
+# Encode the entire text dataset and store it into a torch.Tensor
+data = torch.tensor(encode(text), dtype=torch.long)
+
+# Separate data into train and validation sets
+n = int(0.9*len(data)) # first 90% will be train, the rest will be validation
+train_data = data[:n]
+val_data = data[n:]
+
+# Data loading
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split=='train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+# Supersimple bigram model
 class BigramLanguageModel(nn.Module):
     
     def __init__(self, vocab_size) -> None:
@@ -38,3 +107,29 @@ class BigramLanguageModel(nn.Module):
             # append sampled index to the running sequence
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
+    
+model = BigramLanguageModel(vocab_size)
+m = model.to(device)
+
+# create a PyTorch optimizer
+optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
+
+for iter in range(max_iters):
+    
+    # every once in a while evaluate the loss on train and val sets
+    if iter % eval_interval == 0:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+        
+    # sample a batch of data
+    xb, yb = get_batch('train')
+    
+    # evaluatio the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+    
+# generate from the model
+context = torch.zeros((1,1), dtype=torch.long, device=device)
+print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
